6118b0d0bfe6d66a7bdb85a5af0c8dcd8ca2bcea
Dev-Elie <eochieng9448@gmail.com>
2022-02-04 16:55:51 +0300

    🐛 Fixed env vars

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     database_port: str
     database_password: str
     database_name : str
+    test_database_name: str
     database_username: str
     secret_key : str
     algorithm : str
